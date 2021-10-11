40ecc40e844727833cfff3b2d7c648de3757d767
Charbel <charbel.elhelou2000@gmail.com>
2021-10-11 11:14:38 +0300

    added connection to mongoose database in app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose")
 const userRouter = require('./Routes/user');
+
+const config = require("./Configuration/config")
 
 app.use(express.json({
     extended: true
@@ -9,4 +12,7 @@ app.use(express.json({
 app.use("/user", userRouter);
 
 
-app.listen(3000);
+mongoose.connect(config.connectionString).then(() => {
+    app.listen(3000);
+    console.log("Connected!")
+}).catch(() => { console.log("Error connecting to database..") })
