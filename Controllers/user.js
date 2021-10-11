23fb0a50447ebe605a609b790a0e4e3c1c39d7a2
Charbel <charbel.elhelou2000@gmail.com>
2021-10-11 11:39:12 +0300

    added logic for creating a new user and authenticating + returning an access token

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,11 +1,30 @@
+const UserService = require("../Services/user");
 
 
 
 
 module.exports.signUp = async (req, res, next) => {
-    res.send("Sign Up called!");
+    const userInfo = req.body;
+
+    const createdUser = await UserService.SignUp(userInfo);
+
+    if (!createdUser) {
+        res.send("Error");
+    }
+
+    res.send({ "username": userInfo.email, name: userInfo.fullName });
 }
 
 module.exports.authenticate = async (req, res, next) => {
-    res.send("authenticate called!");
+
+    const userCredentials = req.body;
+
+    let isSuccess = await UserService.Authenticate(userCredentials);
+
+    if (!isSuccess) res.send("Incorrect password or email");
+
+    else {
+        let jwtToken = isSuccess;
+        res.send({ "accessToken": jwtToken });
+    }
 }
--- Services/user.js
+++ Services/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../Configuration/config')
 
 
 const userSchema = new Schema({
@@ -14,9 +16,61 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
     }
 
 });
 
 const User = mongoose.model('User', userSchema, 'users');
+
+module.exports = class UserService {
+
+    static async SignUp(user) {
+
+        const doesUserExist = await this.isEmailValid(user.email);
+
+        if (!doesUserExist) {
+            let newUser = new User();
+            newUser.fullName = user.fullName;
+            newUser.email = user.email;
+            newUser.password = await bcrypt.hash(user.password, 12);
+            newUser.save();
+            return true;
+        }
+
+        else return false;
+    }
+
+
+    static async Authenticate(user) {
+        const userFromDb = await this.isEmailValid(user.email);
+
+        if (!userFromDb) {
+            return false;
+        }
+
+        if (bcrypt.compare(user.password, userFromDb.password) == false) { return false }
+
+        else {
+            let jwtToken = await this.issueJTWToken(userFromDb);
+            return jwtToken;
+        }
+    }
+
+    static async isEmailValid(email) {
+        const existingUser = await User.findOne({ email: email });
+
+        return existingUser;
+    }
+
+    static async issueJTWToken(user) {
+        const token = await jwt.sign({ email: user.email, userId: user._id.toString() },
+            config.appSecret,
+            { expiresIn: '2h' });
+
+
+        return token;
+    }
+
+
+}
