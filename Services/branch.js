4db4d48bb228d9402956e3c2ed06eadc97bc0e7f
Charbel <charbel.elhelou2000@gmail.com>
2021-10-12 11:32:01 +0300

    completed service layer of the BranchService

--- Services/branch.js
+++ Services/branch.js
@@ -21,6 +21,33 @@ const branchSchema = new Schema({
 
 const Branch = mongoose.model('Branch', branchSchema, 'branches');
 
-module.exports = class AddressService { }
+module.exports = class BranchService {
+    static async GetAllBranches() {
+        return await Branch.find();
+    }
+
+
+    static async CreateBranch(branch) {
+
+        let branchToInsert = new Branch();
+        branchToInsert.name = branch.name;
+        branchToInsert.location.coordinates = branch.coordinates;
+
+        return await branchToInsert.save();
+    }
+
+    static async UpdateBranch(id, updatedVersion) {
+        return await Branch.findByIdAndUpdate(id, {
+            name: updatedVersion.name,
+            'location.coordinates': updatedVersion.coordinates
+        }, { new: true });
+    }
+
+    static async DeleteBranch(id) {
+        return await Branch.findByIdAndDelete(id);
+    }
+
+
+}
 
 branchSchema.index({ location: '2dsphere' });
