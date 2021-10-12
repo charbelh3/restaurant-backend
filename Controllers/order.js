3fceb3c4f3de8e91b4f7ac2c4413158ecd4804bf
Charbel <charbel.elhelou2000@gmail.com>
2021-10-12 14:51:37 +0300

    completed createOrder controller function

--- Controllers/order.js
+++ Controllers/order.js
@@ -8,6 +8,10 @@ module.exports.getUserOrders = async (req, res, next) => {
 module.exports.createOrder = async (req, res, next) => {
     let orderToCreate = await OrderService.CreateOrder(req.body, req.userId);
 
+    if (orderToCreate) res.send(orderToCreate);
+
+    else return next(createHttpError(400, 'Cannot create order'));
+
 }
 
 module.exports.updateOrder = async (req, res, next) => {
