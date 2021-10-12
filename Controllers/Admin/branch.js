8912e180114eef15d89aac7db974d3ce2124fd7c
Charbel <charbel.elhelou2000@gmail.com>
2021-10-12 11:38:46 +0300

    completed createBranch function of branch controller

--- Controllers/Admin/branch.js
+++ Controllers/Admin/branch.js
@@ -10,6 +10,11 @@ module.exports.getAllBranches = async (req, res, next) => {
 }
 
 module.exports.createBranch = async (req, res, next) => {
+    let branchToCreate = await BranchService.CreateBranch(req.body);
+
+    if (!branchToCreate) return next(createHttpError(500, "Server error"));
+
+    else res.send(branchToCreate);
 }
 
 module.exports.updateBranch = async (req, res, next) => {
