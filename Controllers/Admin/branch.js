d0a119a657b4907824282a3a23ae0b9e6590a8fa
Charbel <charbel.elhelou2000@gmail.com>
2021-10-12 11:34:04 +0300

    completed getAllbranches controller function

--- Controllers/Admin/branch.js
+++ Controllers/Admin/branch.js
@@ -1,7 +1,12 @@
 const createHttpError = require('http-errors');
-
+const BranchService = require('../../Services/branch');
 
 module.exports.getAllBranches = async (req, res, next) => {
+    let branches = await BranchService.GetAllBranches();
+
+    if (!branches) return next(createHttpError(500, "Server error"))
+
+    else res.send(branches);
 }
 
 module.exports.createBranch = async (req, res, next) => {
