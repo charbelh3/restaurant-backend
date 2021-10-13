c358afe7677b068fea7ad664b5b66003a3d2e7a7
Charbel <charbel.elhelou2000@gmail.com>
2021-10-13 11:45:20 +0300

    Fixed bugs is authorizataion middleware

--- Middlewares/authorization.js
+++ Middlewares/authorization.js
@@ -10,37 +10,43 @@ const UserService = require('../Services/user')
 module.exports.isAuthorizedUser = async (req, res, next) => {
 
     //Check if the request contains an authorization header
-    checkAuth(req);
+    if (!req.get('Authorization')) { next(createHttpError(401, 'Missing or Invalid Token.')); }
+
 
     //getting the token
     const token = req.get('Authorization').split(' ')[1];
 
     //Verify and return the decoded token if valid
-    let decodedToken = verifyAndGetToken(token);
+    let decodedToken = await verifyAndGetToken(token);
+
+    if (!decodedToken) return next(createHttpError(401, 'Missing or Invalid Token.'));
 
 
     //Saving the sender document ID
     req.userId = decodedToken.userId;
+    console.log(req.userId);
 
     //Check if user is disabled or enabled
     let status = await checkUserStatus(decodedToken.userId);
 
     if (status == false) {
-        return next((createHttpError(401, 'Disabled user account')));
+        return next(createHttpError(401, 'Disabled user account'));
     }
 
     else if (status == true) next();
+
 }
 
 
-
-module.exports.isAdmin = (req, res, next) => {
-    checkAuth(req);
+module.exports.isAdmin = async (req, res, next) => {
+    if (!req.get('Authorization')) { next(createHttpError(401, 'Missing or Invalid Token.')); }
 
     //getting the token
     const token = req.get('Authorization').split(' ')[1];
 
-    let decodedToken = verifyAndGetToken(token);
+    let decodedToken = await verifyAndGetToken(token);
+
+    if (!decodedToken) return next(createHttpError(401, 'Missing or Invalid Token.'));
 
     if (decodedToken.role != 'Admin') {
         throw createHttpError(403, 'You do not have enough permissions for that action.');
@@ -51,20 +57,15 @@ module.exports.isAdmin = (req, res, next) => {
     next();
 }
 
-function checkAuth(req) {
-    if (!req.get('Authorization')) {
-        throw (createHttpError(401, 'Missing or Invalid Token.'));
-    }
-}
 
-function verifyAndGetToken(token) {
+async function verifyAndGetToken(token, req, res, next) {
     let decodedToken;
 
     try {
         decodedToken = jwt.verify(token, config.appSecret);
     }
     catch (err) {
-        throw (createHttpError(401, 'Missing or Invalid Token.'));
+        console.log(err);
     }
     return decodedToken;
 }
